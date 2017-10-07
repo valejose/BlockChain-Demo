14dfc05603cf19e0a4bfa86532a12a9c27034eb5
Joseph Valentin <valejose@isu.edu>
2017-10-07 14:34:29 -0600

    Initial Commit - Block Class
    Start of the VS solution and code for the Block Class

---
+++ BlockChain Demo/Block.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace BlockChain_Demo
+{
+    class Block
+    {
+        private int _index;
+        private DateTime _timestamp;
+        private string _data;
+        private string _previousHash;
+        public Block(int index, DateTime timestamp, string data, string previousHash)
+        {
+            _index = index;
+            _timestamp = timestamp;
+            _data = data;
+            _previousHash = previousHash;
+        }
+    }
+}
